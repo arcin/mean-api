2323f072d10a73643debe422419ba02b7af209e0
Charles Arciniega <charlesarcin@gmail.com>
2015-01-26 01:45:30 -0800

    Create a http post route to create new posts using the mongoose save method

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+/***************/
+/*   Modules   */
+/***************/
 /* require necessary modules for application, database, and http connectivity */
+/* require body-parser and morgan for incoming requests and better logging */
 var express = require('express')
 var mongoose = require('mongoose')
 var http = require('http')
+var bodyParser = require('body-parser')
+var logger = require('morgan')
 var app = express()
 
 /* dbUri is the link to the local database, named api, on the default port (27017) */
@@ -31,8 +37,29 @@ var postSchema = new Schema ({
  *  - The name of the collection
  * respectively.
  */
-var Post = dbConnection.model('Post', postSchema, 'posts')
+var Post = dbConnection.model( 'Post', postSchema, 'posts' )
 
+/**************/
+/* Middleware */
+/**************/
+/*
+ * Using Morgan for logging in the dev environment
+ */
+app.use( logger('dev') )
+/*
+ * body parser to parse json data
+ */
+app.use( bodyParser.json() )
+/*
+ * Extended objects can also be parsed from the urlencoded string.
+ * Extended objects meaning nested objects.
+ */
+app.use( bodyParser.urlencoded({ extended: true }) )
+
+
+/**************/
+/*   Routes   */
+/**************/
 /*
  * Create a simple root route
  */
@@ -51,12 +78,32 @@ app.get('/posts', function(req, res){
    * Errors are always the first arguments in node convention.
    * In mongoose the result of the query is the second argument.
    */
-  Post.find({}, function(error, posts){
+  Post.find({}, function(error, posts, next){
     /*
      * Handle errors by yielding to the next middleware in the stack
      */
     if (error) return next(error)
     res.send(posts)
+  })
+})
+
+/*
+ * Create a POST route that allows creation of new post resources.
+ */
+app.post('/posts', function(req, res, next){
+  /*
+   * In mongoose you create the model first and pass in the object with
+   * the appropriate properties
+   */
+  post = new Post(req.body)
+
+  /*
+   * Then you call save() on that object to persist it to the database.
+   * The callback will return the error or the results of the query.
+   */
+  post.save(function(error, results){
+    if (error) return next(error)
+    res.send(results)
   })
 })
 
